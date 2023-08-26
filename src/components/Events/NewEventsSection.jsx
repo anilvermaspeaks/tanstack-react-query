87ea1758818bf9e82beeb92f0e6f473882fe2a30
Anil Verma <anilcs240@gmail.com>
2023-08-26 19:02:35 +0530

    initila setup

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query'
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import EventItem from './EventItem.jsx';
+import { fetchEvents } from '../../util/http.js';
+export default function NewEventsSection() {
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events',],
+    queryFn: fetchEvents,
+    staleTime: 0,
+    //gcTime: 20000// how long data kept in cache
+  });
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock title="An error occurred" message={error?.info?.message || 'Failed to fetch Events'} />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
